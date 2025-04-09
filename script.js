e6abecd6bc4868e966578c49fecc925ae9d9b5a0
xiangshi534 <3182208500@qq.com>
2025-04-09 20:51:50 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
   const musicPaths = [
-    "myweb/shei.mp3",
-    "myweb/shanhuhai.mp3",
+    "/myweb/shei.mp3",
+    "/myweb/shanhuhai.mp3",
     
-    "myweb/xiaohai.mp3",
-    "myweb/jiangnan.mp3",
-    "myweb/huahai.mp3"
+    "/myweb/xiaohai.mp3",
+    "/myweb/jiangnan.mp3",
+    "/myweb/huahai.mp3"
   ];
   let currentMusicIndex = 0;
   let isPlayed = false;
